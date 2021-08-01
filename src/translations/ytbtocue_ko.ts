673d1d061839cb9bb3c9ff5e9ae1f583ca8e7e53
Ilya Kotov <iokotov@astralinux.ru>
2021-08-01 16:09:56 +0300

    added Korean translation (Jung Hee Lee)

---
+++ src/translations/ytbtocue_ko.ts
@@ -0,0 +1,231 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>CueModel</name>
+    <message>
+        <location filename="../cuemodel.cpp" line="43"/>
+        <source>Track</source>
+        <translation>트랙</translation>
+    </message>
+    <message>
+        <location filename="../cuemodel.cpp" line="45"/>
+        <source>Performer</source>
+        <translation>연주기</translation>
+    </message>
+    <message>
+        <location filename="../cuemodel.cpp" line="47"/>
+        <source>Title</source>
+        <translation>제목</translation>
+    </message>
+    <message>
+        <location filename="../cuemodel.cpp" line="49"/>
+        <source>Offset</source>
+        <translation>오프셋</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>YouTube to CUE Converter</source>
+        <translation>YouTube ⇝ CUE 변환기</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="295"/>
+        <source>Download</source>
+        <translation>다운로드</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="306"/>
+        <source>Cancel</source>
+        <translation>취소</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="269"/>
+        <source>Output directory:</source>
+        <translation>출력 디렉토리:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="249"/>
+        <source>Audio format:</source>
+        <translation>오디오 형식:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="321"/>
+        <source>URL:</source>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="331"/>
+        <source>Fetch</source>
+        <translation>가져오기</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="25"/>
+        <source>Cue Sheet</source>
+        <translation>큐 시트</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="33"/>
+        <source>Album:</source>
+        <translation>앨범:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="61"/>
+        <source>File:</source>
+        <translation>파일:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="115"/>
+        <source>Genre:</source>
+        <translation>장르:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="135"/>
+        <source>Date:</source>
+        <translation>날짜:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="155"/>
+        <source>Comment:</source>
+        <translation>해설:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="211"/>
+        <source>Add</source>
+        <translation>추가</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="222"/>
+        <source>Add from text</source>
+        <translation>텍스트에서 추가</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="233"/>
+        <source>Remove</source>
+        <translation>제거</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="279"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="363"/>
+        <source>&amp;File</source>
+        <translation>파일(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="371"/>
+        <source>&amp;Help</source>
+        <translation>도움말(&amp;H)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="386"/>
+        <source>&amp;Exit</source>
+        <translation>종료(&amp;E)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="391"/>
+        <source>About...</source>
+        <translation>정보...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="396"/>
+        <source>About Qt</source>
+        <translation>Qt 정보</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="405"/>
+        <location filename="../mainwindow.cpp" line="278"/>
+        <source>Save CUE As...</source>
+        <translation>다른 이름으로 CUE 저장...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="88"/>
+        <source>N/A</source>
+        <translation>해당 없음</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="75"/>
+        <source>Duration:</source>
+        <translation>재생시간:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="47"/>
+        <source>Album artist:</source>
+        <translation>앨범 아티스트:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="100"/>
+        <source>%1 MiB | %2 KiB/s | ETA: %3</source>
+        <translation>%1 MiB | %2 KiB/s | ETA: %3</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="171"/>
+        <source>Finished</source>
+        <translation>완료됨</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="208"/>
+        <source>Stopped</source>
+        <translation>중지됨</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="219"/>
+        <source>Add track list</source>
+        <translation>트랙 목록 추가</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="302"/>
+        <source>not found</source>
+        <translation>찾을 수 없음</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="304"/>
+        <source>About YouTube to CUE Converter</source>
+        <translation>YouTube ⇝ CUE 변환기 정보</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="305"/>
+        <source>YouTube to CUE Converter %1</source>
+        <translation>YouTube ⇝ CUE 변환기 %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="306"/>
+        <source>This program is intended to download audio albums from &lt;a href=&quot;https://www.youtube.com&quot;&gt;YouTube&lt;/a&gt;. It downloads audio file using &lt;a href=&quot;https://youtube-dl.org&quot;&gt;youtube-dl&lt;/a&gt; and generates Cue Sheet with metadata.</source>
+        <translation>이 프로그램은 &lt;a href=&quot;https://www.youtube.com&quot;&gt;YouTube&lt;/a&gt;에서 오디오 앨범을 다운로드하기 위한 것입니다. &lt;a href=&quot;https://youtube-dl.org&quot;&gt;youtube-dl&lt;/a&gt;을 사용하여 오디오 파일을 다운로드하고 메타데이터가 포함된 큐 시트를 생성합니다.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="309"/>
+        <source>Qt version: %1 (compiled with %2)</source>
+        <translation>Qt 버전: %1 (%2로 컴파일됨)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="312"/>
+        <source>Home page: &lt;a href=&quot;https://github.com/trialuser02/ytbtocue&quot;&gt;https://github.com/trialuser02/ytbtocue&lt;/a&gt;</source>
+        <translation>홈페이지: &lt;a href=&quot;https://github.com/trialuser02/ytbtocue&quot;&gt;https://github.com/trialuser02/ytbtocue&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="323"/>
+        <source>Select Output Directory</source>
+        <translation>출력 디렉토리 선택</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="310"/>
+        <source>youtube-dl version: %1</source>
+        <translation>youtube-dl 버전: %1</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="219"/>
+        <source>Titles and offsets:</source>
+        <translation>제목 및 오프셋:</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="311"/>
+        <source>Written by: Ilya Kotov &amp;lt;iokotov@astralinux.ru&amp;gt;</source>
+        <translation>작성자: Ilya Kotov &amp;lt;iokotov@astralinux.ru&amp;gt;</translation>
+    </message>
+</context>
+</TS>
