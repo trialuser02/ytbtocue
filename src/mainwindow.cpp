c906380828807f25fef31b6f1aab6b730a015a39
Ilya Kotov <iokotov@astralinux.ru>
2021-09-29 19:20:46 +0300

    updated copyrights

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, Ilya Kotov <iokotov@astralinux.ru>
+ * Copyright (c) 2019-2021-2021, Ilya Kotov <iokotov@astralinux.ru>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
