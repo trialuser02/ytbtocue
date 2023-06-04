c0aed106e8d8a5032cbfc15370167094b6b61e6a
Ilya Kotov <iokotov@astralinux.ru>
2023-06-04 19:43:12 +0300

    updated transifex config

--- utils/update_tx.sh
+++ utils/update_tx.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 PROJECT_NAME=ytbtocue
+PROJECT_ORGANIZATION=qmmp-development-team
 TX_CONFIG="../.tx/config"
 
 echo "[main]" > ${TX_CONFIG}
 echo "host = https://www.transifex.com" >> ${TX_CONFIG}
 echo "" >> ${TX_CONFIG}
 
-
 plug_name=${PROJECT_NAME}
 
 echo "Updating ${plug_name}"
 
-echo "[${PROJECT_NAME}.${plug_name}]" >> ${TX_CONFIG}
+echo "[o:${PROJECT_ORGANIZATION}:p:${PROJECT_NAME}:r:${plug_name}]" >> ${TX_CONFIG}
 echo "file_filter = src/translations/${plug_name}_<lang>.ts" >> ${TX_CONFIG}
 echo "source_lang = en" >> ${TX_CONFIG}
 echo "source_file = src/translations/${plug_name}_en.ts" >> ${TX_CONFIG}
@@ -20,10 +20,13 @@ echo "type = QT" >> ${TX_CONFIG}
 echo "" >> ${TX_CONFIG}
 
 
-echo "[${PROJECT_NAME}.${PROJECT_NAME}-desktop]" >> ${TX_CONFIG}
-echo "file_filter = src/desktop-translations/ytbtocue_<lang>.desktop.in" >> ${TX_CONFIG}
+RESOURCE_NAME=${PROJECT_NAME}
+
+echo "Updating ${RESOURCE_NAME}-desktop"
+
+echo "[o:${PROJECT_ORGANIZATION}:p:${PROJECT_NAME}:r:${RESOURCE_NAME}-desktop]" >> ${TX_CONFIG}
+echo "file_filter = src/desktop-translations/${RESOURCE_NAME}_<lang>.desktop.in" >> ${TX_CONFIG}
 echo "source_lang = en" >> ${TX_CONFIG}
-echo "source_file = src/ytbtocue.desktop" >> ${TX_CONFIG}
+echo "source_file = src/${RESOURCE_NAME}.desktop" >> ${TX_CONFIG}
 echo "type = DESKTOP" >> ${TX_CONFIG}
 echo "" >> ${TX_CONFIG}
-
