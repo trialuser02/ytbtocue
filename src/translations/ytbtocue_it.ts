f0f3d70f61ec3a0f17c829d6ebda7fded655722c
Ilya Kotov <iokotov@astralinux.ru>
2024-11-23 20:45:55 +0300

    updated translations

--- src/translations/ytbtocue_it.ts
+++ src/translations/ytbtocue_it.ts
@@ -1,27 +1,27 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="it_IT">
+<TS version="2.1" language="it">
 <context>
     <name>CueModel</name>
     <message>
         <location filename="../cuemodel.cpp" line="43"/>
         <source>Track</source>
-        <translation type="unfinished"></translation>
+        <translation>Traccia</translation>
     </message>
     <message>
         <location filename="../cuemodel.cpp" line="45"/>
         <source>Performer</source>
-        <translation type="unfinished"></translation>
+        <translation>Esecutore/trice</translation>
     </message>
     <message>
         <location filename="../cuemodel.cpp" line="47"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Titolo</translation>
     </message>
     <message>
         <location filename="../cuemodel.cpp" line="49"/>
         <source>Offset</source>
-        <translation type="unfinished"></translation>
+        <translation>Scostamento</translation>
     </message>
 </context>
 <context>
@@ -29,218 +29,218 @@
     <message>
         <location filename="../mainwindow.ui" line="14"/>
         <source>YouTube to CUE Converter</source>
-        <translation type="unfinished"></translation>
+        <translation>Convertitore da YouTube a CUE</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="295"/>
         <source>Download</source>
-        <translation type="unfinished"></translation>
+        <translation>Scarica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="306"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Annulla</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="269"/>
         <source>Output directory:</source>
-        <translation type="unfinished"></translation>
+        <translation>Cartella di output:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="249"/>
         <source>Audio format:</source>
-        <translation type="unfinished"></translation>
+        <translation>Formato audio:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="321"/>
         <source>URL:</source>
-        <translation type="unfinished"></translation>
+        <translation>URL:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="331"/>
         <source>Fetch</source>
-        <translation type="unfinished"></translation>
+        <translation>Recupera</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="25"/>
         <source>Cue Sheet</source>
-        <translation type="unfinished"></translation>
+        <translation>File cue</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="33"/>
         <source>Album:</source>
-        <translation type="unfinished"></translation>
+        <translation>Album:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="61"/>
         <source>File:</source>
-        <translation type="unfinished"></translation>
+        <translation>File:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="115"/>
         <source>Genre:</source>
-        <translation type="unfinished"></translation>
+        <translation>Genere:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="135"/>
         <source>Date:</source>
-        <translation type="unfinished"></translation>
+        <translation>Data:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="155"/>
         <source>Comment:</source>
-        <translation type="unfinished"></translation>
+        <translation>Commento:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="211"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiungi</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="222"/>
         <source>Add from text</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiungi da testo</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="233"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>Rimuovi</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="279"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation>...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="363"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;File</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="371"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Aiuto</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="378"/>
         <source>&amp;Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Modifica</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="393"/>
         <source>&amp;Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Esci</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="398"/>
         <source>About...</source>
-        <translation type="unfinished"></translation>
+        <translation>Informazioni su...</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="403"/>
         <source>About Qt</source>
-        <translation type="unfinished"></translation>
+        <translation>Informazioni su Qt</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="412"/>
         <location filename="../mainwindow.cpp" line="295"/>
         <source>Save CUE As...</source>
-        <translation type="unfinished"></translation>
+        <translation>Salva CUE come:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="420"/>
         <source>&amp;Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Impo&amp;stazioni</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="88"/>
         <source>N/A</source>
-        <translation type="unfinished"></translation>
+        <translation>N/D</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="75"/>
         <source>Duration:</source>
-        <translation type="unfinished"></translation>
+        <translation>Durata:</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="47"/>
         <source>Album artist:</source>
-        <translation type="unfinished"></translation>
+        <translation>Artista dell&apos;album:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="182"/>
         <source>Finished</source>
-        <translation type="unfinished"></translation>
+        <translation>Completato</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="227"/>
         <source>Stopped</source>
-        <translation type="unfinished"></translation>
+        <translation>Fermato</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="238"/>
         <source>Add track list</source>
-        <translation type="unfinished"></translation>
+        <translation>Aggiungi elenco di tracce</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="326"/>
         <source>This program is intended to download audio albums from &lt;a href=&quot;https://www.youtube.com&quot;&gt;YouTube&lt;/a&gt;. It downloads audio file using %1 and generates Cue Sheet with metadata.</source>
-        <translation type="unfinished"></translation>
+        <translation>Lo scopo di questo programma è di scaricare album musicali da &lt;a href=&quot;https://www.youtube.com&quot;&gt;YouTube&lt;/a&gt;. Scarica i file audio usando %1 e genera file CUE con metadati.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="329"/>
         <source>%1 version: %2</source>
-        <translation type="unfinished"></translation>
+        <translation>Versione di %1: %2</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="324"/>
         <source>About YouTube to CUE Converter</source>
-        <translation type="unfinished"></translation>
+        <translation>Informazioni su Convertitore da YouTube a CUE</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="106"/>
         <source>%1 MiB | %2 %3 | ETA: %4</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 MiB | %2 %3 | Rimanente: %4</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="107"/>
         <source>KiB/s</source>
-        <translation type="unfinished"></translation>
+        <translation>KiB/s</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="107"/>
         <source>MiB/s</source>
-        <translation type="unfinished"></translation>
+        <translation>MiB/s</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="325"/>
         <source>YouTube to CUE Converter %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Convertitore da YouTube a CUE %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="328"/>
         <source>Qt version: %1 (compiled with %2)</source>
-        <translation type="unfinished"></translation>
+        <translation>Versione Qt: %1 (compilata con %2)</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="331"/>
         <source>Home page: &lt;a href=&quot;https://github.com/trialuser02/ytbtocue&quot;&gt;https://github.com/trialuser02/ytbtocue&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Pagina principale: &lt;a href=&quot;https://github.com/trialuser02/ytbtocue&quot;&gt;https://github.com/trialuser02/ytbtocue&lt;/a&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="342"/>
         <source>Select Output Directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Seleziona cartella di output</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="238"/>
         <source>Titles and offsets:</source>
-        <translation type="unfinished"></translation>
+        <translation>Titoli e scostamenti:</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="330"/>
         <source>Written by: Ilya Kotov &amp;lt;iokotov@astralinux.ru&amp;gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Scritto da: Ilya Kotov &amp;lt;iokotov@astralinux.ru&amp;gt;</translation>
     </message>
 </context>
 <context>
@@ -248,47 +248,47 @@
     <message>
         <location filename="../settingsdialog.ui" line="14"/>
         <source>YouTube to CUE Converter Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Impostazioni convertitore da YouTube a CUE</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="20"/>
         <source>Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>Proxy</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="32"/>
         <source>Enable proxy usage</source>
-        <translation type="unfinished"></translation>
+        <translation>Abilita uso del proxy</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="57"/>
         <source>Type:</source>
-        <translation type="unfinished"></translation>
+        <translation>Tipo:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="67"/>
         <source>Host name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nome host:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="84"/>
         <source>Port:</source>
-        <translation type="unfinished"></translation>
+        <translation>Porta:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="101"/>
         <source>User name:</source>
-        <translation type="unfinished"></translation>
+        <translation>Nome utente:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="111"/>
         <source>Password:</source>
-        <translation type="unfinished"></translation>
+        <translation>Password:</translation>
     </message>
     <message>
         <location filename="../settingsdialog.ui" line="127"/>
         <source>Extra command-line arguments:</source>
-        <translation type="unfinished"></translation>
+        <translation>Ulteriori argomenti a riga di comando:</translation>
     </message>
 </context>
 </TS>
